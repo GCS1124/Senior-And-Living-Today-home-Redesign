d4527c5a88d0f12eae540ba2a47f8161936e13a8
gcs1124 <info@globalcreativeservices.us>
2026-09-09 12:06:11 +0530

    Remove unnecessary comment from ConsultingPage component

--- src/pages/ConsultingPage.tsx
+++ src/pages/ConsultingPage.tsx
@@ -177,7 +177,6 @@ export function ConsultingPage() {
             </Reveal>
           </Container>
         </section>
-*****/---k
         <section className="bg-ivory-50">
           <Container className="py-16 lg:py-20">
             <Reveal>
